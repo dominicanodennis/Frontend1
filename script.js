f0bd32e21394365123d5146241026a4d4249ce86
dominicanodennis <dominicanodennis@hotmail.de>
2015-07-19 23:52:31 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by dennis on 19.07.15.
+ */
+function rechne(num1) {
+    var result = num1 * num1;
+    document.getElementById('foo2').innerHTML = result + " so ist es";
+    // return result;
+
+
+}
